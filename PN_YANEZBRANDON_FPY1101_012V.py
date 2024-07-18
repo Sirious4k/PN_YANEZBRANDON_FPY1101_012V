54e847f688100631a012fb890d7748c604b9a00e
Brandon Yáñez Ponce <br.yanezp@duocuc.cl>
2024-07-17 20:13:33 -0400

    Tabla de clasificar_sueldos

--- PN_YANEZBRANDON_FPY1101_012V.py
+++ PN_YANEZBRANDON_FPY1101_012V.py
@@ -19,6 +19,37 @@ def generar_sueldos():
     lista_saldos = [(nombre, sueldos_random()) for nombre in trabajadores]
     print("Sueldos aleatorios asignados correctamente.")
 
+# Clasificar sueldos por categorías
+def clasificar_sueldos():
+    global lista_saldos
+    menores_800k = []
+    entre_800k_2m = []
+    mayores_2m = []
+    
+    for nombre, saldo in lista_saldos:
+        if saldo < 800000:
+            menores_800k.append((nombre, saldo))
+        elif saldo >= 800000 and saldo <= 2000000:
+            entre_800k_2m.append((nombre, saldo))
+        else:
+            mayores_2m.append((nombre, saldo))
+    
+    # Ordenar por categorías
+    menores_800k.sort()
+    entre_800k_2m.sort()
+    mayores_2m.sort()
+    
+    # Mostrar resultados
+    print(f"Trabajadores con sueldos menores a $800.000 TOTAL {len(menores_800k)}:")
+    for nombre, saldo in menores_800k:
+        print(f"{nombre}: ${saldo}")
+    print(f"\nTrabajadores con sueldos entre $800.000 y $2.000.000 TOTAL {len(entre_800k_2m)}:")
+    for nombre, saldo in entre_800k_2m:
+        print(f"{nombre}: ${saldo}")
+    print(f"\nTrabajadores con sueldos mayores a $2.000.000 TOTAL {len(mayores_2m)}:")
+    for nombre, saldo in mayores_2m:
+        print(f"{nombre}: ${saldo}")
+
 # Menú
 
 def menu():
