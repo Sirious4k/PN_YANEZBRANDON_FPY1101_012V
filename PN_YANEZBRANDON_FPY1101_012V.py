110ae3e76d911cbf721777b9c832b3f9c349549e
Brandon Yáñez Ponce <br.yanezp@duocuc.cl>
2024-07-17 20:45:46 -0400

    Implementación de sumar_sueldos y ver_estadisticas

--- PN_YANEZBRANDON_FPY1101_012V.py
+++ PN_YANEZBRANDON_FPY1101_012V.py
@@ -1,5 +1,6 @@
 import random
 import csv
+from functools import reduce
 
 # Lista de trabajadores
 trabajadores = ['Juan Pérez','María García','Carlos López','Ana Martínez','Pedro Rodríguez','Laura Hernández',
@@ -41,14 +42,44 @@ def clasificar_sueldos():
     
     # Mostrar resultados
     print(f"Trabajadores con sueldos menores a $800.000 TOTAL {len(menores_800k)}:")
+    print("Nombre empleado  Sueldo")
     for nombre, saldo in menores_800k:
+        print(f"\nNombre empleado  Sueldo")
         print(f"{nombre}: ${saldo}")
     print(f"\nTrabajadores con sueldos entre $800.000 y $2.000.000 TOTAL {len(entre_800k_2m)}:")
+    print(f"\nNombre empleado  Sueldo")
     for nombre, saldo in entre_800k_2m:
         print(f"{nombre}: ${saldo}")
     print(f"\nTrabajadores con sueldos mayores a $2.000.000 TOTAL {len(mayores_2m)}:")
+    print(f"\nNombre empleado  Sueldo")
     for nombre, saldo in mayores_2m:
         print(f"{nombre}: ${saldo}")
+    
+# Sumar todos los sueldos
+def sumar_sueldos():
+    global lista_saldos
+    suma_total = sum(saldo for nombre, saldo in lista_saldos)
+    print(f"\nTOTAL SUELDOS: ${suma_total}")
+    
+def ver_estadisticas():
+    global lista_saldos
+    promedio = sum(saldo for nombre, saldo in lista_saldos) / len(lista_saldos)
+    print(f"\nPromedio sueldo: ${promedio}")
+    
+    maximo = max(saldo for nombre, saldo in lista_saldos)
+    print(f"Sueldo más alto: ${maximo}")
+    
+    minimo = min(saldo for nombre, saldo in lista_saldos)
+    print(f"Sueldo más bajo: ${minimo}")
+    
+    productos = 1
+    n = len(lista_saldos)
+    
+    for nombre, saldo in lista_saldos:
+        productos *= saldo
+    
+    media_geom = productos ** (1 / n)
+    print(f"Media geométrica de los sueldos: ${media_geom:.2f}")
 
 # Menú
 
@@ -60,7 +91,7 @@ def menu():
         print("4. Reporte de sueldos")
         print("5. Salir del programa")
         
-        opcion = int(input("Seleccione una opción (1-5)"))
+        opcion = int(input("Seleccione una opción (1-5): "))
         
         if opcion < 1 or opcion > 5:
             print("Opción inválida. Intente de nuevo.")
@@ -76,6 +107,7 @@ def menu():
         elif opcion == 2:
             print("Clasificando sueldos...")
             clasificar_sueldos()
+            sumar_sueldos()
             
         elif opcion == 3:
             print("Ver estadísticas...")
